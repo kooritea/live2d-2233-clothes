5067829722211d26ab03a74e6329c34927efa48c
kooritea <ochatokori@gmail.com>
2018-05-20 23:00:23 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,26 @@
+# live2d-2233-clothes
+
+> 一个自动爬取B站直播间2233衣服的项目
+
+## 最后发现新衣服时间 2018/05/20 22:51:05
+
+## 列表
+
+closet.2018.lover
+
+closet.2017.summer.normal
+
+closet.2017.valley
+
+closet.2016.xmas
+
+closet.2018.spring
+
+closet.2017.cba-super
+
+closet.2017.tomo-bukatsu.low
+
+closet.2017.newyear
+
+closet.default.v2
+
---
+++ main.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const AppData = require('./lib/AppData.js')
+const bilibili = require('./lib/bilibili.js')
+const saveClothes = require('./lib/saveClothes.js')
+const fs = require('fs')
+
+async function main(){
+  AppData.lastCheckTime()
+  let closet = await bilibili.getAllClothesName()//设置了延迟查询
+  let haveNewClothes = saveClothes.contrastCloset(closet)
+  if(haveNewClothes){
+    //下载了新衣服，准备提交
+    let clothesList = ''
+    for(let clothes of AppData.readData('closet')){
+      clothesList +=`${clothes}\n\n`
+    }
+    let md = `# live2d-2233-clothes\n\n> 一个自动爬取B站直播间2233衣服的项目\n\n## 最后发现新衣服时间 ${AppData.readData('lastCheckTime')}\n\n## 列表\n\n${clothesList}\n\n[保存地址](./dist)`
+    fs.writeFile('./README.md',md)
+  }
+}
+main()
