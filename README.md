20a1c05bdd47847d256fb85637f690cfd1fad21b
kooritea_server <blog@kooritea.cc>
2018-05-20 23:19:24 +0800

    初始化

--- README.md
+++ README.md
@@ -6,28 +6,4 @@
 
 ## 列表
 
-closet.2018.lover
-
-closet.2017.summer.normal
-
-closet.2017.valley
-
-closet.2016.xmas
-
-closet.2018.spring
-
-closet.2017.cba-super
-
-closet.2017.tomo-bukatsu.low
-
-closet.2017.newyear
-
-closet.default.v2
-
-closet.2017.tomo-bukatsu.high
-
-closet.2017.cba-normal
-
-
-
 [保存地址](./dist)
--- main.js
+++ main.js
@@ -16,7 +16,11 @@ async function main(){
       clothesList +=`${clothes}\n\n`
     }
     let md = `# live2d-2233-clothes\n\n> 一个自动爬取B站直播间2233衣服的项目\n\n## 最后发现新衣服时间 ${AppData.readData('lastCheckTime')}\n\n## 列表\n\n${clothesList}\n\n[保存地址](./dist)`
-    fs.writeFile('./README.md',md)
+    fs.writeFile('./README.md',md,(err)=>{
+    	if(err){
+		console.log(err)
+	}
+    })
   }
 }
 main()
